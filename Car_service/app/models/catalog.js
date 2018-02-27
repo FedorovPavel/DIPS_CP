fce5a7e87b5628c76f432fd105b573c5497e82f1
Pavel <UI7.2@yandex.ru>
2018-02-28 00:28:34 +0300

    Сформирована схема БД для сервиса

--- Car_service/app/models/catalog.js
+++ Car_service/app/models/catalog.js
@@ -1,16 +1,44 @@
-// Example model
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ArticleSchema = new Schema({
-  title: String,
-  url: String,
-  text: String
+const CatalogSchema = new Schema({
+  manufacture: {
+    type: String,
+    required: true
+  },
+  model: {
+    type: String
+  },
+  type: {
+    type: String,
+    enum: ['sedan', 'hatchback', 'SUV', 'wagon', 'van', 'coupe', 'minivan', 'other'],
+    required: true,
+    default: 'other'
+  },
+  doors: {
+    type: Number,
+    min: 1,
+  },
+  Person: {
+    type: Number,
+    min: 1,
+    required: true
+  },
+  Images: {
+    type: [String],
+  },
+  rentDate: {
+    type: [{
+      renter: String,
+      from: Date,
+      to: Date
+    }]
+  },
+  cost: {
+    type: Number,
+    required: true,
+    min: 0
+  }
 });
 
-ArticleSchema.virtual('date')
-  .get(() => this._id.getTimestamp());
-
-mongoose.model('Article', ArticleSchema);
-
+mongoose.model('catalog', CatalogSchema);
