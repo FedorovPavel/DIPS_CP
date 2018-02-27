2b4d306bff14d8613e20a0657433134f1ecfa143
Pavel <UI7.2@yandex.ru>
2018-02-27 23:59:10 +0300

    Создан метод для изменения информации об автомобиле

--- Car_service/app/api/controller.js
+++ Car_service/app/api/controller.js
@@ -7,6 +7,12 @@ module.exports = (app) => {
   app.use('/cars', router);
 };
 
+/**
+ * get response template by unification response by server
+ * @param {String} status response status
+ * @param {Object} content information by send
+ * @returns {Object} response information
+ */
 function responseTemplate(status, content) {
   let container = {
     status: status,
@@ -82,8 +88,21 @@ router.post('/newCar', function (req, res, next) {
     if (err) {
       return res.status(400).send(responseTemplate('Error', err));
     }
-    return res.status(200).send(responseTemplate('Ok', car));
+    return res.status(201).send(responseTemplate('Ok', car));
   });
 });
 
 //  update car
+router.put('/update/:id', function (req, res, next) {
+  const id = req.params.id;
+  const updateFields = req.body;
+  if (!updateFields || updateFields == undefined) {
+    return res.status(400).send(responseTemplate('Error', 'Bad request: dont\'t exist update fields'));
+  }
+  return catalog.updateCar(id, updateFields, function (err, car) {
+    if (err) {
+      return res.status(400).send(responseTemplate('Error', err));
+    }
+    return res.status(202).send(responseTemplate('Ok', car));
+  });
+});
