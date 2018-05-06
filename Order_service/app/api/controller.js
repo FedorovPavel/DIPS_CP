22e90fecdff701dcf5a5361008dadb8cc5d9221a
Pavel <UI7.2@yandex.ru>
2018-05-07 01:57:06 +0300

    Добавлены метод изменения состояния заказа с логикой переходов

--- Order_service/app/api/controller.js
+++ Order_service/app/api/controller.js
@@ -4,10 +4,10 @@ const orderManager = require('./../models/order');
 const billingManager = require('./../models/billing');
 
 module.exports = function (app) {
-	app.use('/', router);
+	app.use('/orders', router);
 };
 
-router.get('/orders', function (req, res, next) {
+router.get('/', function (req, res, next) {
 	let page = req.query.page;
 	let count = req.query.count;
 	const id = getUserId(req);
@@ -38,6 +38,97 @@ router.get('/orders', function (req, res, next) {
 	});
 });
 
+router.get('/:id', function (req, res, next) {
+	const uid = getUserId(req);
+	const id = req.params.id;
+	return orderManager.getOrder(uid, id, function (err, order) {
+		if (err) {
+			if (err.kind == 'ObjectId')
+				return res.status(400).send(responseTemplate('Error', { message: 'Bad request : Invalid ID' }));
+			else
+				return res.status(400).send(responseTemplate('Error', err));
+		}
+		if (order) {
+			return res.status(200).send(responseTemplate('Ok', order));
+		}
+		return res.status(404).send(responseTemplate('Error', { message: "Order isn't found" }));
+	});
+});
+
+router.post('/', function (req, res, next) {
+	let item = {
+		userId: getUserId(req),
+		carId: req.body.carID,
+		startDate: req.body.startDate,
+		endDate: req.body.endDate
+	};
+	return orders.createOrder(item, function (err, result) {
+		if (err)
+			return res.status(400).send(responseTemplate('Error', err));
+		if (result) {
+			return res.status(201).send(responseTemplate('Ok', result));
+		}
+		return res.status(500).send(responseTemplate('Ok', { message: "Order don't create" }));
+	});
+});
+
+router.put('/:id/confirm', function (req, res, next) {
+	const id = req.params.id;
+	const uid = getUserId(req);
+	return orders.changeOrderStatus(uid, id, 1, function (err, result) {
+		if (err) {
+			if (err.kind == "ObjectId")
+				return res.status(400).send(responseTemplate('Error', { message: 'Bad request: bad ID' }));
+			else
+				return res.status(400).send(responseTemplate('Error', { message: err }));
+		} else {
+			if (result) {
+				const data = {
+					content: result,
+					service: scope
+				};
+				return res.status(200).send(data);
+			}
+			return res.status(404).send(responseTemplate('Error', { message: 'Not found order' }));
+		}
+	});
+});
+
+router.put('/:id/paid', function (req, res, next) {
+	const id = req.params.id;
+	const uid = getUserId(req);
+	const info = req.body;
+	return orderManager.changeOrderStatus(uid, id, 2, function (err, result) {
+		if (err) {
+			if (err.kind == "ObjectId")
+				return res.status(400).send(responseTemplate('Error', { message: 'Bad request:Bad ID' }));
+			else
+				return res.status(400).send(responseTemplate('Error', err));
+		}
+		if (result) {
+			return res.status(200).send(responseTemplate('Ok', result));
+		}
+		return res.status(404).send(responseTemplate('Error', { message: 'Order not found' }));
+	}, info);
+});
+
+router.put('/:id/complete', function (req, res, next) {
+	const id = req.params.id;
+	const uid = getUserId(req);
+	return orderManager.changeOrderStatus(uid, id, 3, function (err, result) {
+		if (err) {
+			if (err.kind == "ObjectId")
+				return res.status(400).send(responseTemplate('Error', { message: 'Bad ID' }));
+			else
+				return res.status(400).send(responseTemplate('Error', err));
+		}
+		if (result) {
+			return res.status(202).send(responseTemplate('Ok', result));
+		}
+		return res.status(404).send(responseTemplate('Error', { message: 'Not found order' }));
+	});
+});
+
 /**
  * get response template by unification response by server
  * @param {String} status response status
@@ -50,4 +141,9 @@ function responseTemplate(status, content) {
 		content: content
 	};
 	return container;
+}
+
+function getUserId(req) {
+	const id = req.headers['userid'];
+	return id;
 }
--- Order_service/app/models/order.js
+++ Order_service/app/models/order.js
@@ -3,22 +3,59 @@ const mongoose = require('mongoose'),
 	Billing = require('./billing');
 
 const OrderSchema = new Schema({
-	userId: Schema.Types.ObjectId,
-	carId: Schema.Types.ObjectId,
+	userId: {
+		type: Schema.Types.ObjectId,
+		required: true
+	},
+	carId: {
+		type: Schema.Types.ObjectId,
+		required: true
+	},
 	billing: Billing.Schema,
-	status: String,
+	status: {
+		type: String,
+		required: true
+	},
 	cost: {
 		type: Number,
 		required: true
 	},
 	lease: {
-		startDate: Date,
-		endDate: Date
+		startDate: {
+			type: Date,
+			required: true
+		},
+		endDate: {
+			type: Date,
+			required: true
+		}
 	},
 	created: {
-		type: Number
+		type: Number,
+		required: true
 	}
 });
+
+OrderSchema.statics.createOrder = function (objectInfo, callback) {
+	let object = Object(objectInfo);
+	const check = checkRequiredFields(Object.keys(object));
+	if (check) {
+		let order = createOrder(object);
+		if (order) {
+			return order.save(function (err, result) {
+				if (err)
+					return callback(err, null);
+				else {
+					return callback(null, result.toObject());
+				}
+			});
+		} else {
+			return callback('Unknown fields', null);
+		}
+	} else {
+		return callback('not found required fields', null);
+	}
+};
 
 OrderSchema.statics.getOrders = function (userId, page = 0, count = 20, callback) {
 	page = Number(page);
@@ -40,13 +77,101 @@ OrderSchema.statics.getOrders = function (userId, page = 0, count = 20, callback
 	}).skip(page * count).limit(count);
 };
 
-OrderSchema.statics.getCount = function(uid, callback){
-  return this.count({userId : uid}, function(err, count){
-    if (err)
-      return callback(err, null);
-    return callback(null, count);
-  });
+OrderSchema.statics.getOrder = function (uid, id, callback) {
+	return this.findOne({ _id: id, userId: uid }, function (err, result) {
+		if (err)
+			return callback(err, null);
+		else {
+			if (result) {
+				return callback(null, result.toObject());
+			} else {
+				return callback(null, null);
+			}
+		}
+	});
+};
+
+OrderSchema.statics.getCount = function (uid, callback) {
+	return this.count({ userId: uid }, function (err, count) {
+		if (err)
+			return callback(err, null);
+		return callback(null, count);
+	});
 }
+
+OrderSchema.statics.setConfirm = function (uid, id, callback) {
+	return this.findOne({ _id: id, userId: uid }, function (err, order) {
+		if (err)
+			return callback(err, null);
+		else {
+			if (order) {
+				if (order.status == 'Draft') {
+					order.Status = 'Confirm';
+					return order.save(function (err, res) {
+						if (err)
+							return callback(err, null);
+						else
+							return callback(null, res);
+					});
+				} else {
+					return callback({ message: "Status don't right" }, null);
+				}
+			} else {
+				return callback(null, null);
+			}
+		}
+	});
+};
+
+OrderSchema.statics.setPaid = function (uid, id, info, callback) {
+	return this.findOne({ _id: id, userId: uid }, function (err, order) {
+		if (err)
+			return callback(err, null);
+		else {
+			if (order) {
+				if (order.status == 'WaitForBilling') {
+					order.status = 'Paid';
+					order.billing = info;
+					return order.save(function (err, res) {
+						if (err)
+							return callback(err, null);
+						else
+							return callback(null, res.toObject());
+					});
+				} else {
+					return callback({ message: "Status don't right" }, null);
+				}
+			} else {
+				return callback(null, null);
+			}
+		}
+	});
+};
+
+OrderSchema.statics.setComplete = function (uid, id, callback) {
+	return this.findOne({ _id: id, userId: uid }, function (err, order) {
+		if (err)
+			return callback(err, null);
+		else {
+			if (order) {
+				if (order.status == 'Paid') {
+					order.status = 'Completed';
+					return order.save(function (err, res) {
+						if (err)
+							return callback(err, null);
+						else {
+							return callback(null, res.toObject());
+						}
+					});
+				} else {
+					return callback({ message: "Status don't right" }, null);
+				}
+			} else {
+				return callback(null, null);
+			}
+		}
+	});
+};
 
 OrderSchema.methods.toObject = function () {
 	const item = {
@@ -69,12 +194,67 @@ const orderORM = mongoose.model('Order', OrderSchema);
 const manager = new class {
 
 	getOrders(id, page, count, callback) {
-		return orderORM.getOrder(id, page, count, callback);
+		return orderORM.getOrders(id, page, count, callback);
 	}
 
-	getCount(id) {
-		return orderORM.getCount(id, callback);
+	getOrder(id, callback) {
+		return orderORM.getOrder(id, callback);
+	}
+
+	getCount(uid, id, callback) {
+		return orderORM.getCount(uid, id, callback);
+	}
+
+	createOrder(objectInfo, callback) {
+		return orderORM.createOrder(objectInfo, callback);
+	}
+
+	changeOrderStatus(uid, id, state, callback, info = undefined) {
+		switch (state) {
+			case 1: {
+				return orderORM.setConfirm(uid, id, callback);
+			}
+			case 2: {
+				return orderORM.setPaid(uid, id, info, callback);
+			}
+			case 3: {
+				return orderORM.setComplete(uid, id, callback);
+			}
+			default: {
+				return callback({ message: "unknown operation" });
+			}
+		}
 	}
 }();
 
 module.exports = manager;
+
+function createOrder(object) {
+	const model = mongoose.model('Order');
+	let item = new model();
+	let errorParse = false;
+	for (key in object) {
+		switch (key) {
+			case 'userId':
+				item.userId = mongoose.Types.ObjectId(object[key]);
+				break;
+			case 'carId':
+				item.carId = mongoose.Types.ObjectId(object[key]);
+				break;
+			case 'startDate':
+				item.lease.startDate = new Date(object[key]);
+				break;
+			case 'endDate':
+				item.lease.endDate = new Date(object[key]);
+				break;
+			default:
+				errorParse = true;
+				break;
+		}
+	}
+	if (errorParse)
+		return null;
+	item.created = Date.now();
+	item.Status = 'Draft';
+	return item;
+}
